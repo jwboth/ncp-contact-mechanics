410a783863916e834e2b58ebef71d4366e8e1ff0
Jakub Both <jakub.both@uib.no>
2025-08-08 23:02:40 +0200

    extract auxiliary computations

--- src/ncp/__init__.py
+++ src/ncp/__init__.py
@@ -9,9 +9,9 @@ from .functions import *
 from .fracture_states import *
 from .iteration_export import *
 from .solution_strategy import *
-from .contact_mechanics import *
 from .metric import *
 from .nonlinear_solvers import *
 from .utils import *
 from .numerics import *
 from .statistics import *
+from .auxiliary import *
